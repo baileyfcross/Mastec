95d48ca7edfb4bdf34ee7869e413884480f61a70
baileyfcross <bf02cros@siena.edu>
2022-04-04 14:00:53 -0400

    initial creation

--- functions.js
+++ functions.js
@@ -1,3 +1,4 @@
+/*
 function submitButton(){
   $(document).ready(() => {
 
@@ -16,8 +17,77 @@ function submitButton(){
     });
   });
 }
+*/
+function test() {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', function(e) {
+  var response = e.target.responseText;
+  console.log(response);
+  });
+  xhr.addEventListener('error', function(e) {
+  console.error('Request error with status', e.target.status);
+  });
+  xhr.open('GET', 'https://dev.mastecat.net/test/cooking/recipes/Alex');
+  xhr.setRequestHeader('Authorization','Basic QkNyb3NzOnh4WW4xMn5+ITE=');
+  xhr.send();
+}
+/*
+function simpAPI(){
+  $(document).ready(() => {
+    var data;
+    $.ajax({
+       url : 'https://dev.mastecat.net/test/cooking/pantry/Alex',
+       method : 'GET',
+       beforeSend: function (xhr) {
+         xhr.setRequestHeader("authorization", "Basic QkNyb3NzOnh4WW4xMn5+ITE=");
+         xhr.setRequestHeader('Content-Type', 'application/xml');
+         xhr.setRequestHeader('Access-Control-Request-Method', 'GET');
+       },
+       dataType: 'json',
+       contentType: 'application/json; charset=utf-8',
+       data: JSON.stringify(data),
+       success : function(result){
+         console.log("Successful AJAX Call")
+       },
+       error : function(result, statut, error){
+         console.log("Unsuccessful AJAX Call")
+         console.log(error);
+       }
+    });
+  });
+}
 
+*/
+function simpAPI2(){
 
+  $(document).ready(() => {
+    var data;
+    $.ajax({
+       url : 'https://dev.mastecat.net/test/cooking/pantry/Alex',
+       xhrFields: {
+        withCredentials: true
+      },
+       beforeSend: function (xhr) {
+         xhr.setRequestHeader("Access-Control-Request-Method", "GET");
+         xhr.setRequestHeader("Access-Control-Request-Headers", "Authorization");
+         //xhr.setRequestHeader('Content-Type', 'application/xml');
+         //xhr.setRequestHeader("Authorization", "Basic QkNyb3NzOnh4WW4xMn5+ITE=");
+       },
+       headers: function (xh) {
+         xh.setRequestHeader("Authorization", "Basic QkNyb3NzOnh4WW4xMn5+ITE=");
+       },
+       crossDomain: true,
+       success : function(result){
+         console.log("Successful AJAX Call")
+       },
+       error : function(result, statut, error){
+         console.log("Unsuccessful AJAX Call")
+         console.log(error);
+       }
+    });
+  });
+}
+/*
 function fetchMethod() {
   console.log("Executed before get request")
   var myBody = '';
@@ -27,9 +97,7 @@ function fetchMethod() {
       method: 'GET',
       body: myBody,
       headers: {
-      'Content-Type': 'application/json',
-      'username' : 'BCross',
-      'password' : 'xxYn12~~!1'
+        "Authentication": "Basic QkNyb3NzOnh4WW4xMn5+ITE="
     },
     mode: 'no-cors'
   });
@@ -49,13 +117,11 @@ function oldXML() {
   };
 
   xhttp.open("GET", "https://dev.mastecat.net/test/cooking/pantry/Alex", true);
-  xhttp.setRequestHeader("Content-type", "application/json");
-  xhttp.setRequestHeader("username", "BCross");
-  xhttp.setRequestHeader("password", "xxYn12~~!1");
+  xhttp.setRequestHeader("Authentication", "Basic QkNyb3NzOnh4WW4xMn5+ITE=");
   console.log(xhttp.getAllResponseHeaders())
   xhttp.send();
 }
-
+/*
 function simpleGet()
 {
     var xmlHttp = new XMLHttpRequest();
@@ -116,28 +182,20 @@ function wCred() {
   });
 }
 
-function example(){
-  // Create a request variable and assign a new XMLHttpRequest object to it.
-  var request = new XMLHttpRequest()
 
-  // Open a new connection, using the GET request on the URL endpoint
-  request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-
-  request.onload = function () {
-    // Begin accessing JSON data here
-    var data = JSON.parse(this.response)
-
-    if(request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-      console.log(movie.title)
-    })
-  } else {
-    console.log('error')
+function callOtherDomain() {
+  const invocation = new XMLHttpRequest();
+  const url = 'https://dev.mastecat.net/test/cooking/recipes/Alex';
+    if (invocation) {
+      invocation.open('GET', url, true);
+      invocation.withCredentials = true;
+      invocation.onreadystatechange = function() {
+           if (invocation.readyState == 4 && invocation.status == 200) {
+               alert(invocation.responseText);
+           };
+      invocation.send();
+    }
   }
-  }
-
-  // Send request
-  request.send()
 }
 
 
@@ -164,3 +222,4 @@ function example(){
   // Send request
   request.send()
 }
+*/
---
+++ phpApi.php
@@ -0,0 +1,16 @@
+<?php
+public function phpApiTest()
+{
+  $post = [
+      'username' => 'user1',
+      'password' => 'passuser1',
+      'gender'   => 1,
+  ];
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, '');
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+  $response = curl_exec($ch);
+  var_export($response);
+}
+ ?>
